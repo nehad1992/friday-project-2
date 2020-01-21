8d6db2790f54383b0de3a828add605d9f1ed3bea
neha doodi <nehadoodipoonia@gmail.com>
2020-01-20 18:15:01 -0800

    add branching, multiple else if conditions

--- programming-language/js/scripts.js
+++ programming-language/js/scripts.js
@@ -1,23 +1,26 @@
 $(document).ready(function() {
     $("#form1").submit(function() {
-        // var age = $("input#formInfo2-input").val();
-        // if  (age > "20") {
-        //     alert("You should choose Ruby");
-        // }  
-        // else  
-        // {
-        //     alert("You should wait");
-        // } 
-        
+        var season = $("input:radio[name=season]:checked").val();
+        var drink = $("input:radio[name=drink]:checked").val();
+        var sports = $("input:radio[name=sports]:checked").val();
+        var vacation = $("input:radio[name=vacation]:checked").val();
         var color = $("input:radio[name=color]:checked").val();
-        if  (color === "white")  {
+        if  (color === "white" && season === "summer" && drink === "coffee" && sports === "swimming" && vacation === "city")  
+        {
              
             alert("You should choose java");
         }
-        else  {
+        else if ( season === "white" && color === "grey" && drink === "kombucha" && sports === "rugby") 
+         {
             alert("You should choose C#");
         }
-        
+        else if (season === "spring" && drink === "tea" && sports === "cricket")
+        {
+            alert("You should choose Python");
+        }
+        else {
+            alert("You should choose Ruby");
+        }
         event.preventDefault();
 
     });
